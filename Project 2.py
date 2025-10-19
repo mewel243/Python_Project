e9a8e1a88f43311cb078d4e2ccd355b0f1077d9e
mewel243 <jmm3286@mavs.uta.edu>
2025-10-19 14:53:05 -0500

    Add files via upload

---
+++ Project 2.py
@@ -0,0 +1,33 @@
+'''
+
+NAME: Mewel Mbalanda
+DATE: 29 Sep 24
+PROJ: Two
+DESC: Basic Calculator Program 
+
+'''
+# This is input
+number1 = float(input ("what is your first number?"))
+number2 = float(input ("What is your second number?")) 
+
+# This is processing
+add = number1 + number2 
+sub = number1 - number2 
+mult = number1 * number2 
+div = number1 / number2 
+mod = number1 % number2 
+
+
+# This is output... displaying the result to the user
+print ("\n\n\n")
+print (number1, "+", number2, "=", add)
+print (number1, "-", number2, "=", sub)
+print (number1, "*", number2, "=", mult)
+print (number1, "/", number2, "=", div)
+print (number1, "%", number2, "=", mod)
+
+
+
+                  
+
+
